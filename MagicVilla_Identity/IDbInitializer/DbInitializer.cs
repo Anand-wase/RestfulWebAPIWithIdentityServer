bc8170208eeb6f230f19e3661b90934d2aacb7bd
anandvardhan.pandi <anandvardhan.pandi@AB-LPT-VJY-123>
2023-08-22 10:11:28 +0530

    Add project files.

---
+++ MagicVilla_Identity/IDbInitializer/DbInitializer.cs
@@ -0,0 +1,63 @@
+﻿using IdentityModel;
+using MagicVilla_Identity.Data;
+using MagicVilla_Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace MagicVilla_Identity.IDbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DbInitializer(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db=db;
+            _userManager=userManager;
+            _roleManager=roleManager;  
+        }
+        public void Initialize()
+        {
+            if (_roleManager.FindByNameAsync(SD.Admin).Result == null)
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Customer)).GetAwaiter().GetResult();
+            }
+            else { return; }
+            ApplicationUser adminUser = new ApplicationUser()
+            {
+                UserName = "admin1@gmail.com",
+                Email = "admin1@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "1111111111",
+                Name = "Ben"
+            };
+            _userManager.CreateAsync(adminUser, "Admin@123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(adminUser,SD.Admin).GetAwaiter().GetResult();
+            var claims1 = _userManager.AddClaimsAsync(adminUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name,adminUser.Name),
+                new Claim(JwtClaimTypes.Role,SD.Admin)
+            }).Result;
+
+
+            ApplicationUser customerUser = new ApplicationUser()
+            {
+                UserName = "customer1@gmail.com",
+                Email = "customer1@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "1111111111",
+                Name = "Ben Customer"
+            };
+            _userManager.CreateAsync(customerUser, "Admin@123*").GetAwaiter().GetResult();
+            _userManager.AddToRoleAsync(adminUser, SD.Admin).GetAwaiter().GetResult();
+            var temp2 = _userManager.AddClaimsAsync(customerUser, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name,customerUser.Name),
+                new Claim(JwtClaimTypes.Role,SD.Customer)
+            }).Result;
+
+        }
+    }
+}
---
+++ MagicVilla_Identity/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace MagicVilla_Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public String Name { get; set; } 
+    }
+}
